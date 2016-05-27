599200d7a27717ec1bdfdf0d0f9f281252f2b48d
Vivien Kuo <lost.angel.fin@gmail.com>
2016-05-26 21:54:54 -0500

    finish queue

--- queue_spec.rb
+++ queue_spec.rb
@@ -1,5 +1,44 @@
 require_relative 'queue'
 
-describe Queue do
+describe 'Queue' do
+	 let(:queue) { Queue.new }
+  it 'can create a new queue of 1 element' do
+    queue.enqueue(2)
+    expect(queue.length).to eq 1
+  end
+
+  it 'can enqueue 3 elements' do
+    queue.enqueue(2)
+    queue.enqueue(5)
+    queue.enqueue("yay")
+    expect(queue.length).to eq 3
+  end
+
+  it 'can dequeue first of queue' do
+    queue.enqueue(2)
+    queue.enqueue(5)
+    queue.enqueue("yay")
+    queue.dequeue
+    expect(queue.peel).to eq 5
+  end
+
+  it 'can dequeue and return' do
+    queue.enqueue(2)
+    queue.enqueue(5)
+    queue.enqueue("yay")
+    expect(queue.dequeue).to eq 2
+  end
+
+  it 'can return but not remove first of the queue' do
+    queue.enqueue(2)
+    queue.enqueue(5)
+    queue.enqueue("yay")
+    queue.peel
+    expect(queue.peel).to eq 2
+  end
+
+  it 'can check if the queue is empty' do
+    expect(queue.empty?).to eq true
+  end
 
 end
